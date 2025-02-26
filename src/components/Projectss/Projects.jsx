f61555e1d46a355745d9ea4a181b4d7fd749b8ee
Umair Shaikh <imumairshaikh07@gmail.com>
2025-02-26 14:59:21 +0530

    made changes in the project card

--- src/components/Projectss/Projects.jsx
+++ src/components/Projectss/Projects.jsx
@@ -20,6 +20,7 @@ import { motion } from "framer-motion";
 import "./Projects.css";
 import react from "../../assets/react.svg";
 import tailwind from "../../assets/tailwincss.svg";
+import { IoIosLink } from "react-icons/io";
 
 const projects = [
   {
@@ -51,6 +52,7 @@ const projects = [
     imgURL: geekFoody,
     github: "https://github.com/umairr07/GeekFoods",
     goLive: "https://geek-foods.vercel.app/",
+    techstack: [react, tailwind],
   },
   {
     id: 2,
@@ -60,6 +62,7 @@ const projects = [
     imgURL: pix,
     github: "https://github.com/umairr07/PixTransformer",
     goLive: "https://pix-transformer.vercel.app/",
+    techstack: [react, tailwind],
   },
   {
     id: 3,
@@ -69,6 +72,7 @@ const projects = [
       "Interview Ready with AI generates customized resumes and recommends job opportunities based on your skills.",
     github: "https://github.com/umairr07/Resume-Ai",
     goLive: "https://umairr07.github.io/Resume-Ai/",
+    techstack: [HTML_URL, CSS_URL, JS_URL],
   },
 ];
 
@@ -92,7 +96,7 @@ function Projects() {
               whileInView={{ opacity: 1, y: 0 }}
               initial={{ opacity: 0, y: 100 }}
               transition={{ duration: 1 }}
-              className="group flex flex-col justify-start items-start gap-2 lg:w-[500px] md:w-[400px] sm:w-[330px] lg:h-[300px] sm:h-[250px] duration-500 relative rounded-lg p-4 bg-slate-100 hover:-translate-y-2 hover:shadow-lg hover:shadow-slate-500 cursor-pointer sm:mr-10 lg:mr-0"
+              className="group flex flex-col justify-start items-start gap-2 lg:w-[500px] md:w-[400px] sm:w-[330px] lg:h-[300px] sm:h-[300px] duration-500 relative rounded-lg p-4 bg-slate-100 hover:-translate-y-2 hover:shadow-lg hover:shadow-slate-500 cursor-pointer sm:mr-10 lg:mr-0"
             >
               <div
                 className=" absolute duration-700 shadow-md group-hover:-translate-y-4 group-hover:-translate-x-4 -bottom-10 -right-10 lg:w-[350px] lg:h-1/2 rounded-lg bg-purple-400 md:w-[280px] sm:w-[200px] sm:h-[90px]"
@@ -107,23 +111,32 @@ function Projects() {
                     <h2 className="text-2xl font-bold mb-2 ">
                       {project.title}
                     </h2>
-                    <div>{}</div>
+                  </div>
+                  <div className="flex gap-2 py-3">
+                    {project?.techstack?.map((tech) => (
+                      <div key={tech}>
+                        <img src={tech} alt="" className="w-6 h-6" />
+                      </div>
+                    ))}
                   </div>
                   <p className="">{project.description}</p>
                 </div>
-                <div className="flex gap-3 mt-5">
-                  <button className="Btn">
-                    <FaGithub className="svgIcon" />
-                    <a href={project.github} className="text" target="blank">
-                      Github
-                    </a>
-                  </button>
-                  <button className="Btn">
-                    <FaLink className="svgIcon" />
-                    <a href={project.goLive} className="text" target="blank">
-                      Go Live
-                    </a>
-                  </button>
+                <div className="flex gap-2 mt-5">
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="p-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition"
+                  >
+                    <FaGithub className="text-2xl text-gray-700 hover:text-black" />
+                  </a>
+
+                  <a
+                    href={project.goLive}
+                    target="_blank"
+                    className="p-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition"
+                  >
+                    <IoIosLink className="text-2xl text-gray-700 hover:text-black" />
+                  </a>
                 </div>
               </div>
             </motion.div>
